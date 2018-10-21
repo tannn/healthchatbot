c433345817639f53d55f472cd657455f04b47c1e
Jordan Barker <Jordanbarker5@gmail.com>
2018-10-20 19:30:39 -0500

    redid chat data once again

--- website.py
+++ website.py
@@ -2,6 +2,7 @@ from rasa_core.channels.socketio import SocketIOInput
 from rasa_core.agent import Agent
 from rasa_core.interpreter import RasaNLUInterpreter
 
-interpreter = RasaNLUInterpreter("models/current/nlu/")
-agent = Agent.load("models/current/dialogue", interpreter=interpreter)
+interpreter = RasaNLUInterpreter("models/second/nlu/")
+
+agent = Agent.load("models/second/dialogue", interpreter=interpreter)
 agent.handle_channels([SocketIOInput()], 5500, serve_forever=True)
